e707841f6569d895d076e778b9419d8a89e6ba7d
Leto <mikhaylichenko2000@bk.ru>
2022-08-08 12:37:50 +0300

    Разные вырианты нахождения квадрата

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿Console.Clear();
 //  Задача на квадрат числа
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine()); 
 
-Console.WriteLine("Квадрат = " + number*number);
+Console.WriteLine("Квадрат числа " + number +" = "+ number*number);
+
+// int result = Convert.ToInt32(Math.Pow(number, 2));
+// Console.WriteLine("Квадрат числа = " + result);
+
